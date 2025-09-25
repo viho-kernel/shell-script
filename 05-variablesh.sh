bcbd93a0441d26bf9fa08ee7c049268624778ef3
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-25 20:31:12 +0530

    9th commit

--- 05-variablesh.sh
+++ 05-variablesh.sh
@@ -2,7 +2,7 @@
 
 echo "Enter Username and Password as command line arguments"
 
-read -s "USERNAME is: " USERNAME
-read -s "PASSWORD is: " PASSWORD
+read -s "USERNAME is: USERNAME"
+read -s "PASSWORD is: PASSWORD"
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
