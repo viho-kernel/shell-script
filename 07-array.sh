d9e855400ee4fab5e9035d49755c4b4c3a7840e4
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-25 21:19:53 +0530

    9th commit

--- 07-array.sh
+++ 07-array.sh
@@ -4,3 +4,4 @@ FRUITS=("Mango" "Apple" "Banana" "Grapes")
 echo "First Fruit: ${FRUITS[0]}"
 echo "Second Fruit: ${FRUITS[1]}"
 echo "All Fruits: ${FRUITS[@]}"
+echo "Total number of fruits: ${#FRUITS[@]}"
