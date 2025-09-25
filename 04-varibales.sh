46763336f7ff55f4b2edb4693f883af10336545c
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-25 20:14:21 +0530

    7th commit

---
+++ 04-varibales.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2:: Hello $PERSON1, Good Morning"
+echo "$PERSON1:: How are you?"
+echo "$PERSON2:: I am fine, What about you?"
+echo "$PERSON1:: I am also fine"
+echo "$PERSON1:: What are you doing these days?"  
+echo "$PERSON2:: I am learning Shell Scripting"
+echo "$PERSON1:: Oh! That's great"
+echo "$PERSON2:: Yes, It is very interesting" 
+echo "$PERSON1:: Good Bye"
+echo "$PERSON2:: Good Bye"
+echo "$PERSON1:: Take Care"
+echo "$PERSON2:: You too"
+echo "$PERSON1:: See you later"
+echo "$PERSON2:: Sure"
+echo "$PERSON1:: Bye"
+echo "$PERSON2:: Bye"
+echo "$PERSON1:: Have a nice day"
+echo "$PERSON2:: You too, Have a nice day"
