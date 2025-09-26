f4aa3eda9f76ae27c6f66c66e0572efc60b8a70e
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-26 13:35:49 +0530

    another commit

--- mysql.sh
+++ mysql.sh
@@ -4,10 +4,10 @@ id=$(id -u)
 if [ $id -ne 0 ]
 then
     echo "You must be a root user to run this script"
+    exit 1
 else
     echo "You are a root user"
 fi
-
-exit 0
-
 yum install mysql -y
+
+echo "mysql --version"
