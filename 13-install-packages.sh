d9903221798e7444b08c0942440de0058cefd8fa
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-29 11:37:13 +0530

    latest commit

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,16 +6,44 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started execution at $TIMESTAMP" &>>$LOGFILE
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then
+      echo -e "$R Error:: $2 installation failed.$N"
+      exit 1
+  else
+      echo -e "$G Success:: $2 installed successfully.$N"
+  fi
+
+}
 if [ $ID -ne 0 ] 
 then    
   echo -e "$R Error:: This script must be run as root or with sudo.$N"
   exit 1
 else
-    echo -e "$G You are running this script as root."
+    echo -e "$G You are running this script as root.$N"
 
 fi
 
 echo -e "$G All arguments passed: $@"
+
+for package in $@
+do 
+    yum list installed $package &>>$LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y &>>$LOGFILE #install package
+        VALIDATE $? "Installation of $package" #validate installation
+    else
+        echo -e "$Y Warning:: $package is already installed, skipping...$N"
+    fi
+
+done
 
 
 
