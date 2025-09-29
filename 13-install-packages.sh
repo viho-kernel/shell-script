e40b3e3579d8605da919c3978abc58b10e6faf2f
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-29 10:55:29 +0530

    latest commit

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,9 +2,9 @@
 # Install necessary packages for user management
 # Run this script with sudo or as root
 ID=$(id -u)
-R=$\e[31m
-G=$\e[32m
-N=$\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 if [ "$ID" -ne 0 ] 
 then    
   echo "$R Error:: This script must be run as root or with sudo.$N"
