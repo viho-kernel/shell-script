9fc6a11f7b6e699376e6d85a633662f0b0b4e36e
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-02 06:58:52 +0530

    latest commit

---
+++ delete-older-logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shell-script"
+
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "Source directory $SOURCE_DIR does not exist."
+  exit 1
+fi
+
+FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -name "*.log" -mtime +7)
+
+while IFS= read -r line
+do
+    echo "Deleting the file $line"
+    rm -f "$line"
+done <<<"$FILES_TO_DELETE"
+
