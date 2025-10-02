87866ae417e1c340edb051dd36305a25887466a0
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-02 08:35:03 +0530

    latest commit

--- usage.sh
+++ usage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -ht | grep -vE 'tmp|File')
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=10
 message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print $1F}')
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="High Disk Utilization on $partition: $usage"   
